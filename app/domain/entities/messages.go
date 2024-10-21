15f04049eef2151d4c571d9055533f3166031b47
MBI <59748306+MBI-88@users.noreply.github.com>
2024-10-21 07:00:05 -0300

    working with grpc

--- app/domain/entities/messages.go
+++ app/domain/entities/messages.go
@@ -6,5 +6,5 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 type Message struct {
 	ID      primitive.ObjectID `bson:"_id,omitemtpy" json:"id,omitempty"`
 	Topic   string             `bson:"topic" json:"topic" validate:"required,min=3,max=5"`
-	Payload []byte             `bson:"payload" json:"payload" validate:"required,max=1000"`
+	Payload []byte             `bson:"payload" json:"payload" validate:"required,max=65535"`
 }
--- app/interfaces/grpc/input/controllers.go
+++ app/interfaces/grpc/input/controllers.go
@@ -1,1 +1,67 @@
 package input
+
+import (
+	"context"
+	"dominus/app/interactors"
+	pb "dominus/app/interfaces/grpc/proto/builder"
+
+	jsoniter "github.com/json-iterator/go"
+)
+
+
+type grpc struct {
+	pb.UnimplementedAPIServer
+	inter interactors.InteractorInt
+	js    jsoniter.API
+}
+
+//Receives simple messages from client
+func (s *grpc) SendLocal(_ context.Context, ms *pb.Message) (*pb.Response, error) {
+	var response *pb.Response
+
+	return response, nil
+}
+
+//Receives array messages from client
+func (s *grpc) SendClientStreamLocal(stream pb.API_SendClientStreamLocalClient) error {
+
+	return nil
+}
+
+//Sends array messages to client
+func (s *grpc) SendServerStreamLocal(ms *pb.Message, stream pb.API_SendServerStreamLocalServer) error {
+
+	return nil
+}
+
+//Receives and sends messages from server to client
+func (s *grpc) SendStreamLocal(stream pb.API_SendStreamLocalServer) error {
+
+	return nil
+}
+
+
+
+func (s *grpc) SendRemote(_ context.Context, ms *pb.Message) (*pb.Response, error) {
+	var response *pb.Response
+
+	return response, nil
+}
+
+func (s *grpc) SendClientStreamRemote(stream pb.API_SendClientStreamRemoteClient) error {
+
+	return nil
+}
+
+
+func (s *grpc) SendServerStreamRemote(ms *pb.Message, stream pb.API_SendServerStreamRemoteServer) error {
+
+	return nil
+}
+
+
+func (s *grpc) SendStreamRemote(stream pb.API_SendStreamRemoteServer) error {
+
+	return nil
+}
+
