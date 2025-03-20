655fb1e43455f8e7416a4de0300d13c40d4918bc
MBI-88 <ingmbi8807@gmail.com>
2025-03-20 08:11:39 -0300

    fixed header in interceptors

--- app/interfaces/grpc/middlewares/interceptors.go
+++ app/interfaces/grpc/middlewares/interceptors.go
@@ -13,14 +13,14 @@ type interceptor struct {
 
 func (i *interceptor) UnaryAuthInterceptor(ctx context.Context, method string, req, reply any, cc *grpc.ClientConn,
 	invoker grpc.UnaryInvoker, opts ...grpc.CallOption) error {
-	ctx = metadata.AppendToOutgoingContext(ctx, "API_TOKEN", i.apiToken)
+	ctx = metadata.AppendToOutgoingContext(ctx, "x-api-key", i.apiToken)
 	err := invoker(ctx, method, req, reply, cc, opts...)
 	return err
 }
 
 func (i *interceptor) StreamAuthInterceptor(ctx context.Context, desc *grpc.StreamDesc, cc *grpc.ClientConn, method string,
 	streamer grpc.Streamer, opts ...grpc.CallOption) (grpc.ClientStream, error) {
-	ctx = metadata.AppendToOutgoingContext(ctx, "API_TOKEN", i.apiToken)
+	ctx = metadata.AppendToOutgoingContext(ctx, "x-api-key", i.apiToken)
 	s, err := streamer(ctx, desc, cc, method, opts...)
 	if err != nil {
 		return nil, err
