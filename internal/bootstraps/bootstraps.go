da9bfde404a03ac8be8c3cd0404171ec561abe07
MBI-88 <ingmbi8807@gmail.com>
2026-04-15 21:39:50 -0300

    Revert "fix: fixed in bootstrap create group in redis action 2"
    This reverts commit 1b04492c1bb6343396e492e69b0137900336dac4.

--- internal/bootstraps/bootstraps.go
+++ internal/bootstraps/bootstraps.go
@@ -143,7 +143,7 @@ func gRPServer(
 	// Listener
 	listener, err := net.Listen("tcp", fmt.Sprintf("0.0.0.0:%d", cf.GrpcConfig.Port))
 	if err != nil {
-		logs.WriteLog(context.Background(), enum.ERROR, "net.Listen", err.Error())
+		log.Println(err)
 		return nil
 	}
 	go func(sr *grpc.Server, list net.Listener, cancel context.CancelFunc) {
